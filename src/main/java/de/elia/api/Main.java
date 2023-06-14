09bc7502e15f7fc5978cafd4e669d8d9ff054d49
Elia1802 <timlucas.huth@outlook.de>
2023-06-14 16:10:43 +0200

    register the TheZepserAPI

--- src/main/java/de/elia/api/Main.java
+++ src/main/java/de/elia/api/Main.java
@@ -1,15 +1,28 @@
 package de.elia.api;
 
+import de.elia.api.logging.PluginLogger;
+import de.elia.api.thezepserapi.components.DamageBlocked;
+import de.elia.api.thezepserapi.components.FlightBlocked;
+import de.elia.api.thezepserapi.spells.spells.gravitation.GRAVITATION_SUPER;
+import de.elia.api.thezepserapi.spells.spells.weather.WEATHER_DEFENSE;
+import de.elia.api.thezepserapi.spells.spells.weather.WEATHER_SUPER;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public class Main extends JavaPlugin {
 
    private static Main soulMain;
+   private final PluginLogger logger = new PluginLogger("Soul-Library");
 
    @Override
    public void onEnable(){
       soulMain = this;
+      Bukkit.getPluginManager().registerEvents(new FlightBlocked(), this);
+      Bukkit.getPluginManager().registerEvents(new DamageBlocked(), this);
+      Bukkit.getPluginManager().registerEvents(new GRAVITATION_SUPER(), this);
+      Bukkit.getPluginManager().registerEvents(new WEATHER_SUPER(), this);
+      Bukkit.getPluginManager().registerEvents(new WEATHER_DEFENSE(), this);
    }
 
    @Override
@@ -19,12 +32,18 @@ public class Main extends JavaPlugin {
 
    /**
     * @author Elia
-    * @since V. 1.0
-    * @version V. 1.0
+    * @since V.1.0.0
+    * @version V.1.0.0
     * @description the instance of this plugin
     */
    @NotNull
    public static Main soulMain() {
       return soulMain;
    }
+
+   @NotNull
+   public PluginLogger logger(){
+      return this.logger;
+   }
+
 }
